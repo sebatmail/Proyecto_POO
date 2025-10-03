b3a8bd50a8dcecbfe5fbff1636c0f1a25e1fddf3
sebastian <sebatmail@gmail.com>
2025-10-03 18:10:54 -0300

    tablas

--- data/DDL03.10.2025_17.30.sql
+++ data/DDL03.10.2025_17.30.sql
@@ -1,11 +1,12 @@
 USE iei_172_n2;
 
 CREATE TABLE IF NOT EXISTS guest(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_guest CHAR(5) NOT NULL,
+    id INTEGER AUTO_INCREMENT,
     name_guest VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL, 
     email   VARCHAR(50) NOT NULL,
     phone   VARCHAR (20) NOT NULL,
+
+    CONSTRAINT pk_guest PRIMARY KEY (id)
 );
 
